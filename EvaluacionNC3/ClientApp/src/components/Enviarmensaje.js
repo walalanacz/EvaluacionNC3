135563f5c1a5f0843fdb81bbbfaaa674fa8b0e9f
Nicolas Campillay <nicocampillayzavala@gmail.com>
2022-07-21 21:52:23 -0400

    Agregar archivos de proyecto.

---
+++ EvaluacionNC3/ClientApp/src/components/Enviarmensaje.js
@@ -0,0 +1,171 @@
+﻿import React, { useState, useEffect } from 'react'
+
+const usuariosDefinidos = [
+    { nombre: "Pepe Lota", like: true, saludo: false, mensaje: "Todo bien" },
+    { nombre: "Tulio Triviño", like: false, saludo: false, mensaje: "Aqui les envio un saludo" }
+]
+
+const Enviarmensaje = () => {
+
+    const [usuarios, setUsuarios] = useState(usuariosDefinidos)
+    const [nombre, setNombre] = useState("")
+    const [mensaje, setMensaje] = useState("")
+    const [like, setLike] = useState(false)
+    const [saludo, setSaludo] = useState(false)
+
+    useEffect(() => {
+        let clonaUsuarios = [...usuarios]
+        const recienteUsuario = {
+            nombre: "Patana Triviño",
+            like: true,
+            saludo: true,
+            mensaje: "Hola buenas soy patana"
+        }
+        clonaUsuarios.push(recienteUsuario)
+        setUsuarios(clonaUsuarios)
+    }, [])
+
+    const handleNombre = (evento) => {
+        setNombre(evento.target.value)
+    }
+
+    const handleMensaje = (evento) => {
+        setMensaje(evento.target.value)
+    }
+
+    const handleLike = (evento) => {
+        setLike(!like)
+    }
+
+    const handleSaludo = (evento) => {
+        setSaludo(!saludo)
+    }
+
+    const handleSendMensaje = (evento) => {
+        const recienteUsuario = {
+            nombre: nombre,
+            like: like,
+            saludo: saludo,
+            mensaje: mensaje,
+        }
+
+        let clonaUsuarios = [...usuarios]
+        clonaUsuarios.push(recienteUsuario)
+        setUsuarios(clonaUsuarios)
+
+    }
+
+    return (
+        <>
+            <div className="row">
+                <h1>Enviar Mensaje</h1>
+                <div className="col-6">
+                    <label htmlFor="nombres">Nombre</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        value={nombre}
+                        onChange={handleNombre}
+                        name="nombres"
+                        id="nombres"
+                    />
+                </div>
+            </div>
+
+
+            <div className="row-4 mt-2">
+                <div className="col-6">
+                    <label htmlFor="mensaje">Mensaje</label>
+                    <textarea
+                        className="form-control"
+                        type="text"
+                        value={mensaje}
+                        onChange={handleMensaje}
+                        name="nombres"
+                        id="nombres"
+                        rows="3"
+                    />
+                </div>
+            </div>
+            <div className="row mt-4">
+                <div className="col-3">
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value={like}
+                            onChange={handleLike}
+                            name="like"
+                            id="like"
+                        />
+                        <label className="form-check-label" htmlFor="like">
+                            Dar Like 👍
+                        </label>
+                    </div>
+                </div>
+
+                <div className="col-3">
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value={saludo}
+                            onChange={handleSaludo}
+                            name="saludo"
+                            id="saludo"
+                        />
+                        <label className="form-check-label" htmlFor="saludo">
+                            Enviar Saludo 👋
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row mt-4">
+                <div className="col-4">
+                    <div className="col-12 justify-content-start mt-3">
+                        <button
+                            className="btn btn-primary"
+                            type="button"
+                            onClick={handleSendMensaje}
+                        >Enviar</button>
+                    </div>
+                </div>
+            </div>
+
+            <hr />
+
+
+
+            <div className="row mt-5">
+                <div className="col-12">
+                    <table className="table table-stripped">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Reacciones</th>
+                                <th>Mensaje</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {usuarios.map((usuario) => {
+                                return (
+                                    <tr>
+                                        <td>{usuario.nombre}</td>
+                                        <td>
+                                            {usuario.like && '👍'}
+                                            {usuario.saludo && '👋​​'}
+                                            {!usuario.like && !usuario.saludo && '<Ninguno>'}
+                                        </td>
+                                        <td>{usuario.mensaje}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Enviarmensaje
---
+++ EvaluacionNC3/ClientApp/src/components/Portada.js
@@ -0,0 +1,52 @@
+﻿import React, { useState, useEffect } from 'react'
+import imagenPortada from '../assets/images/motonc.jpg'
+
+
+const Portada = () => {
+    return (
+        <>
+            <div className="row">
+                <div className="col-6">
+                    <div className="card">
+                        <img src={imagenPortada} class="card-img-top" height='500'/>
+                        <div className="card-body">
+                            <h5 className="card-title">Estudiante Tecnico</h5>
+                            <p className="card-text">
+                                Tecnico Analista Programador 2020-2022
+                                <br></br>
+                                Universidad Inacap
+                                <br></br>
+                                Estudiante trabajador
+                                <br></br>
+                                Progresion 95%
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-6 text-center">
+                    <h1>Nicolás Campillay</h1>
+                    <p>
+                        Soy un Estudiante que empezo en el año 2020, con clases online a travez de plataformas teams,
+                        adquiriendo habilidades de codificación, analisis de empresas con sus problematicas con alguna
+                        solucion y de soporte en sistema, hardware y componentes dando mantencion a los equipos
+                    </p>
+                </div>
+            </div>
+
+            <div className="row mt-4">
+                <div className="col-6">
+                    <h1>Habilidades</h1>
+                    <ul className="list-group">
+                        <li className="list-group-item">Analisis de la situación de la empresa con el mercado</li>
+                        <li className="list-group-item">Coperativo en equipos de trabajo</li>
+                        <li className="list-group-item">Buen trato a los clientes</li>
+                        <li className="list-group-item">Excelente gestión de tiempos</li>
+                    </ul>
+
+                </div>
+            </div>
+        </>
+    )
+}
+export default Portada
